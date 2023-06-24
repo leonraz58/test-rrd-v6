8a16a307023a1bcb250f8718ba6e40f2e2a12aee
leonraz58 <leonraz58@gmail.com>
2023-06-24 20:55:32 +0300

    04 links

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {NavLink, Routes, Route, Outlet, useParams} from "react-router-dom";
 
-const Profile = () => {
-    const params = useParams<'*'>()
-
-    const some = params['*']
-
-    console.log(some)
-
-    return <div>profile</div>
-}
 
 function App() {
     return (
         <div className="App">
             <NavLink to={'/'}>main</NavLink>---
             <NavLink to={'/login'}>login</NavLink>---
-            <NavLink to={'/profile'}>profile</NavLink>---
-            <NavLink to={'/profile/1'}>profile/1</NavLink>---
+            <NavLink
+                to={'/profile'}
+                style={(params) => {
+                    return {color: params.isActive ? 'lime' : 'black'}
+                }}
+            >profile</NavLink>---
+
+            <NavLink to={'/profile/settings'}
+                     className={({isActive}) => isActive ? 'act' : 'def'}
+            >settings</NavLink>---
 
             <Routes>
                 <Route path={'/*'} element={<div>404</div>}/>
                 <Route path={'/'} element={<div>main</div>}/>
                 <Route path={'/login'} element={<div>login</div>}/>
-                <Route path={'/profile/*'} element={<div><Profile/></div>}/>
+                <Route path={'/profile'} element={<div>profile</div>}/>
+                <Route path={'/profile/settings'} element={<div>settings</div>}/>
             </Routes>
+
+            <a href="https://github.com/leonraz58"
+               target={'_blank'}
+               rel={'noreferrer nofollow noopener'}
+            >xxx</a>
         </div>
     );
 }
