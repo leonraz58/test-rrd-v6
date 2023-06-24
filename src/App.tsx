6f7c9125306499db1d48960612e596e015f3d594
leonraz58 <leonraz58@gmail.com>
2023-06-24 19:26:44 +0300

    03 useParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {NavLink, Routes, Route, Outlet} from "react-router-dom";
+import {NavLink, Routes, Route, Outlet, useParams} from "react-router-dom";
 
+const Profile = () => {
+    const params = useParams<'*'>()
+
+    const some = params['*']
+
+    console.log(some)
+
+    return <div>profile</div>
+}
 
 function App() {
     return (
@@ -10,36 +19,13 @@ function App() {
             <NavLink to={'/'}>main</NavLink>---
             <NavLink to={'/login'}>login</NavLink>---
             <NavLink to={'/profile'}>profile</NavLink>---
-            <NavLink to={'/profile/settings'}>settings</NavLink>---
-            <NavLink to={'/profile2'}>profile2</NavLink>---
-            <NavLink to={'/profile2/settings2'}>settings2</NavLink>---
+            <NavLink to={'/profile/1'}>profile/1</NavLink>---
 
             <Routes>
                 <Route path={'/*'} element={<div>404</div>}/>
                 <Route path={'/'} element={<div>main</div>}/>
                 <Route path={'/login'} element={<div>login</div>}/>
-                <Route path={'/profile/*'} element={(
-                    <div>
-                        profile
-                        <Routes>
-                            <Route path={'/settings'} element={<div>settings</div>}/>
-                        </Routes>
-                    </div>
-                )}
-                />
-                <Route path={'/profile2'} element={(
-                    <div>
-                        profile2
-                        <Outlet/>
-                    </div>
-                )}>
-                    <Route path={'*'} element={<div>profile page not found</div>}/>
-                    <Route index element={<div>check id</div>}/>
-                    <Route path={':id/:x'} element={<div>id</div>}/>
-                    <Route path={'settings2'} element={<div>settings2</div>}/>
-                </Route>
-
-
+                <Route path={'/profile/*'} element={<div><Profile/></div>}/>
             </Routes>
         </div>
     );
